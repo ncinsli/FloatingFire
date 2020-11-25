616f3d5dbc61b3520d02e9344fc8cf9158baae65
Василий Гайсен <56553344+ncinsli@users.noreply.github.com>
2020-11-25 11:14:48 +0500

    Some light selecting fix

--- Assets/Scripts/Selecting/Selectable.cs
+++ Assets/Scripts/Selecting/Selectable.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 //Обычный селект объекта цветом, суть в том, что таких вариантов выбора объектов может быть много. Потому имплементируем ISelectable
 [RequireComponent(typeof(SpriteRenderer))]
 public class Selectable : MonoBehaviour, ISelectable{
     [SerializeField] private Color selectedColor;
+    [Space] [SerializeField] private UnityEvent onSelects;  
+    [SerializeField] private UnityEvent onDeselects;
     protected SpriteRenderer spriteRenderer;
     protected Color originColor;
     private void Awake() => spriteRenderer = GetComponent<SpriteRenderer>();
@@ -13,9 +16,11 @@ public class Selectable : MonoBehaviour, ISelectable{
     public void Select(){
         originColor = spriteRenderer.color;
         spriteRenderer.color = selectedColor;
+        onSelects?.Invoke();
     }
 
     public void Deselect(){
         spriteRenderer.color = originColor;
+        onDeselects?.Invoke();
     }
 }
